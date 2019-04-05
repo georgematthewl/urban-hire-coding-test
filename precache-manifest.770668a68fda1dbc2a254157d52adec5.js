0720ead037efa7fb6c569a172f6d995bdd33a499
George Matthew Limongan <matthew.limongan@gmail.com>
2019-04-05 16:59:19 +0700

    Updates

---
+++ precache-manifest.770668a68fda1dbc2a254157d52adec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1219298de0b372ee18c",
+    "url": "/urban-hire-coding-test/static/js/main.af09a3df.chunk.js"
+  },
+  {
+    "revision": "76b01f904f3a995a94d4",
+    "url": "/urban-hire-coding-test/static/js/runtime~main.3e868598.js"
+  },
+  {
+    "revision": "de98339677206264a97b",
+    "url": "/urban-hire-coding-test/static/css/2.29574c01.chunk.css"
+  },
+  {
+    "revision": "de98339677206264a97b",
+    "url": "/urban-hire-coding-test/static/js/2.a69ec812.chunk.js"
+  },
+  {
+    "revision": "d4f874285a84b81bc8043835ec670695",
+    "url": "/urban-hire-coding-test/static/media/bg.d4f87428.jpg"
+  },
+  {
+    "revision": "2443aeb529f78a022ac7acb3edc0c3c8",
+    "url": "/urban-hire-coding-test/static/media/logo.2443aeb5.svg"
+  },
+  {
+    "revision": "52b674c01d21f69c384dd794b74f4bf1",
+    "url": "/urban-hire-coding-test/index.html"
+  }
+];
