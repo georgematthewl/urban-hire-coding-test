3a7a7ebaa3ed9c906729b8c4150eba4c02d958c2
George Matthew Limongan <matthew.limongan@gmail.com>
2019-04-05 17:01:34 +0700

    Updates

--- src/containers/App/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const AppContainer = styled.div`
-    font-family: 'Fira Sans', sans-serif;
-    position: relative;
-    background-color: #fff;
-    min-height: 100vh;
-`;
